c54fac14df118cdaf13cbd5e1a09f55db426b9bc
bgomez31 <bryan.gomez1311@gmail.com>
2021-12-17 14:11:01 -0400

    [MOD] Restringir usuarios Vendedores

--- mod_ext/hide_buttons/__manifest__.py
+++ mod_ext/hide_buttons/__manifest__.py
@@ -1,20 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     'name': "hide buttons",
     'summary': """ """,
     'description': """ """,
     'author': "INM & LDR Soluciones Tecnológicas y Empresariales C.A",
     'website': "http://www.yourcompany.com",
-    'contribuitors': "Bryan Gómez <bryan.gomez1311@gmail.com>,  Ing Johan Morey",
+    'contribuitors': "Bryan Gómez <bryan.gomez1311@gmail.com>",
     'category': "Extra Tools",
     'version': '0.1',
-    'depends': ['sale', 'account'],
+    'depends': ['base', 'sale', 'account', 'purchase'],
     'data': [
         'security/account_security.xml',
         'views/sale_views.xml',
         'views/account_views.xml',
+        'views/account_payment_views.xml',
+        'views/res_partner_views.xml',
     ],
     'installable': True,
     'application': False,
     'auto_install': False,
 }
+# -*- coding: utf-8 -*-
